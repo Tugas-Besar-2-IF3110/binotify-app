9ceb5f783eea2fd6e100907c5eb8d8573212dceb
davidkarelh <kareldavid9@gmail.com>
2022-11-30 11:52:34 +0700

    Fix subscriptionmodel update method

--- app/models/subscriptionmodel.php
+++ app/models/subscriptionmodel.php
@@ -21,17 +21,29 @@ class subscriptionmodel {
         return $this->db->multiple();
     }
 
-    function update_status($creator_id, $subscriber_id, $status) {
-        $query = "UPDATE subscription " .
-        "SET status = '". $status ."' " .
-        "WHERE " .
-        "creator_id = " . $creator_id .
-        " AND subscriber_id = " . $subscriber_id
-        ;
-        $this->db->query($query);
-        $this->db->execute();
+    function get_subscription_by_subscriber_id_and_creator_id($subscriberId, $creatorId) {
+        $this->db->query('SELECT * FROM subscription WHERE subscriber_id = ' . $subscriberId . ' AND creator_id = ' . $creatorId);
+        return $this->db->single();
+    }
 
-        return $this->db->rowCount() == 1;
+    function update_status($creator_id, $subscriber_id, $status) {
+        $subscription = $this->get_subscription_by_subscriber_id_and_creator_id($subscriber_id, $creator_id);
+
+        if ($subscription != null && isset($subscription["status"]) && $subscription["status"] == $status) {
+            return true;
+        } else {
+            $query = "UPDATE subscription " .
+            "SET status = '". $status ."' " .
+            "WHERE " .
+            "creator_id = " . $creator_id .
+            " AND subscriber_id = " . $subscriber_id
+            ;
+            $this->db->query($query);
+            $this->db->execute();
+    
+            return $this->db->rowCount() == 1;
+
+        }
     }
 }
 
